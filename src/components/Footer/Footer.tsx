db7e2acdeee6a1adef453fb2b262887b8cc11a2c
XeniyaMV <mehnuchka.xd@gmail.com>
2024-03-20 15:28:55 -0700

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import ContainerLayout from '../ContainerLayout';
+
+const Footer = (): JSX.Element => (
+  <footer className="footer">
+    <ContainerLayout className="footer__content">
+      <p>Copyright © 2024 Xeniya Gazizova</p>
+    </ContainerLayout>
+  </footer>
+);
+
+export default Footer;
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,5 +1,6 @@
 import AboutMe from '../../components/AboutMe';
 import Contact from '../../components/Contact';
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import Projects from '../../components/Projects';
 import StartScreen from '../../components/StartScreen';
@@ -12,6 +13,7 @@ const MainPage = (): JSX.Element => (
       <AboutMe />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   </>
 );
